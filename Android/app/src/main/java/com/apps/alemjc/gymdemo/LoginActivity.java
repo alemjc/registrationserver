6868f9bd617f40b791c4073fd660155b7826940e
Daniel Wu <dw352934@gmail.com>
2018-09-29 00:13:41 -0400

    Added bottom navigation tag

--- Android/app/src/main/java/com/apps/alemjc/gymdemo/LoginActivity.java
+++ Android/app/src/main/java/com/apps/alemjc/gymdemo/LoginActivity.java
@@ -1,26 +1,29 @@
 package com.apps.alemjc.gymdemo;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity{
 
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: starting.");
     }
 
 //    This method needs to be substituted for the workout activity
-    public void startWorkoutActivity(View view) {
-        Intent intent = new Intent(this, WorkoutActivity.class);
+    public void startFilterActivity(View view) {
+        Intent intent = new Intent(this, FilterActivity.class);
         startActivity(intent);
     }
 }
